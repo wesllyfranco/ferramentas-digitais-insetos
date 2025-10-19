8f4101e365b2c2adf1f4e840c9164dfa6553f026
wesllyfranco <101141158+wesllyfranco@users.noreply.github.com>
2025-10-19 18:51:04 -0300

    .

---
+++ flashcards/jogo_flashcard/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Biologia de Insetos',
+    'Quantas pernas tem um inseto?',
+    'Seis pernas'
+)
+
+criaCartao(
+    'Biologia de Insetos',
+    'Como se chama o processo de transformação de larva para adulto em insetos?',
+    'Metamorfose'
+)
+
+criaCartao(
+    'Curiosidades de Insetos',
+    'Qual inseto é famoso por produzir mel?',
+    'Abelha'
+)
+
+criaCartao(
+    'Ciência',
+    'Como se chama o estudo científico dos insetos?',
+    'Entomologia'
+)
+
+criaCartao(
+    'Biologia de Insetos',
+    'Qual o nome do revestimento externo e duro do corpo de um inseto?',
+    'Exoesqueleto'
+)
+
+criaCartao(
+    'Classificação',
+    'A qual ordem científica pertencem as borboletas e mariposas?',
+    'Lepidoptera'
+)
